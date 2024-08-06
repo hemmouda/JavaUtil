db3b7e1c9b8f7d1bb7ef1c34d686996d4ea9b770
HEMMOUDA Aymane <ahemmouda@gmail.com>
2024-08-06 05:37:06 +0200

    ADD: Node.java

---
+++ src/main/java/hemmouda/util/structures/Node.java
@@ -0,0 +1,39 @@
+package hemmouda.util.structures;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * A simple node structure to
+ * construct a tree with.
+ * Fields are publicly accessible.
+ */
+public class Node <T> {
+
+    /**
+     * The parent of this node
+     */
+    public Node <T> parent;
+    /**
+     * The value itself of this node
+     */
+    public T value;
+    /**
+     * Children of this node
+     */
+    public List<Node <T>> children;
+
+    public Node (Node<T> parent, T value, List<Node <T>> children) {
+        this.parent = parent;
+        this.value = value;
+        this.children = children;
+    }
+
+    public Node (Node<T> parent, T value) {
+        this(parent, value, new LinkedList<>());
+    }
+
+    public Node () {
+        this(null, null);
+    }
+}
