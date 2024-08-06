4efc4e8ce312d24940454fa0a616ac9b13dad6f9
HEMMOUDA Aymane <ahemmouda@gmail.com>
2024-08-06 05:40:16 +0200

    ADD: UtilTest.java

---
+++ src/test/java/UtilTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UtilTest {
+
+    @Test
+    public void basicTest () {
+        // Tf you want me to test in that?
+    }
+
+}
