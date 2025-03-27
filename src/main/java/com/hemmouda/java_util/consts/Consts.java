4af44404f069e69892e234dd6d2603538f28a1bb
HEMMOUDA Aymane <ahemmouda@gmail.com>
2025-03-27 20:41:53 +0100

    ADD: Consts.java

---
+++ src/main/java/com/hemmouda/java_util/consts/Consts.java
@@ -0,0 +1,9 @@
+package com.hemmouda.java_util.consts;
+
+public class Consts {
+
+    public static final String DIGITS = "0123456789";
+
+    public static final String LETTERS = "abcdefghijklmnopqrstuvwxyz";
+
+}
