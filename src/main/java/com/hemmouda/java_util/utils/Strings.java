a222cf019a6ce00fb174ee84fbdb667d81147015
HEMMOUDA Aymane <ahemmouda@gmail.com>
2025-03-27 20:41:59 +0100

    ADD: Strings.java

---
+++ src/main/java/com/hemmouda/java_util/utils/Strings.java
@@ -0,0 +1,20 @@
+package com.hemmouda.java_util.utils;
+
+/**
+ * String utils
+ */
+public class Strings {
+
+    /**
+     * @return the capitalized version of <code>s</code>. Or
+     * <code>null</code> if <code>s</code> is <code>null</code>.
+     */
+    public static String capitalize (String s) {
+        if (s == null) {
+            return null;
+        }
+
+        return s.substring(0, 1).toUpperCase() + s.substring(1).toLowerCase();
+    }
+
+}
