9c19b72a663090112d056a610135262948e403b5
HEMMOUDA Aymane <ahemmouda@gmail.com>
2025-04-29 11:04:21 +0200

    FIX Strings.java

--- src/main/java/com/hemmouda/java_util/utils/Strings.java
+++ src/main/java/com/hemmouda/java_util/utils/Strings.java
@@ -12,6 +12,8 @@ public class Strings {
     public static String capitalize (String s) {
         if (s == null) {
             return null;
+        } else if (s.isEmpty()) {
+            return s;
         }
 
         return s.substring(0, 1).toUpperCase() + s.substring(1).toLowerCase();
